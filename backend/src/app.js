9643c9c514561a3d3562f880a8465c9ada3ccb2f
gamoorim <gabrielrock012@gmail.com>
2026-05-02 00:10:19 -0300

    alteração no banco de dados e conexão

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,12 @@ const routers = require('./routers/routers')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: ['https://noowaystore.vercel.app', 'http://localhost:3000'],
+  credentials: false
+}))
+
 app.use(routers)
 app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')))
 
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -7,7 +7,8 @@ const knex = require('knex')({
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PWD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        ssl: { rejectUnauthorized: false }
     }
 })
 
