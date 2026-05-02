6df3a9ad8a7f696bdd33c90ba5f8d5fab7afce97
gamoorim <gabrielrock012@gmail.com>
2026-05-02 01:47:19 -0300

    config para vercel

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -2,19 +2,22 @@ const path = require('path')
 const fs = require('fs')
 const multer = require('multer')
 
-const uploadDir = path.resolve(__dirname, '..', 'uploads')
-fs.mkdirSync(uploadDir, { recursive: true })
+const isVercel = process.env.VERCEL === '1'
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir)
-    },
-    filename: (req, file, cb) => {
-        const nomeUnico = Date.now() + '-' + file.originalname
-        cb(null, nomeUnico)
-    }
-})
+let upload
 
-const upload = multer({ storage })
+if (isVercel) {
+  upload = multer({ storage: multer.memoryStorage() })
+} else {
+  const uploadDir = path.resolve(__dirname, '..', 'uploads')
+  fs.mkdirSync(uploadDir, { recursive: true })
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, uploadDir),
+    filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+  })
+
+  upload = multer({ storage })
+}
 
 module.exports = upload
